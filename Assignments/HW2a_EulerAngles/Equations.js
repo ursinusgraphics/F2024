b348263c2b0247a4916f846bd44822428dca4ee1
Chris Tralie <chris.tralie@gmail.com>
2024-09-25 16:58:35 -0400

    Adding thumbnail previews of meshes

--- Assignments/HW2a_EulerAngles/Equations.js
+++ Assignments/HW2a_EulerAngles/Equations.js
@@ -1,8 +1,8 @@
 var rotXEq = document.getElementById('rotXEq');
-rotXEq.innerHTML = "\\[ R_X(\\gamma) = \\left[ \\begin{array}{ccc} 1 & 0 & 0 \\\\ 0 & \\cos(\\gamma) & -\\sin(\\gamma)  \\\\ 0 & \\sin(\\gamma) & \\cos(\\gamma) \\end{array}\\right] \\]";
+rotXEq.innerHTML = "\\[ R_X(\\beta) = \\left[ \\begin{array}{ccc} 1 & 0 & 0 \\\\ 0 & \\cos(\\beta) & -\\sin(\\beta)  \\\\ 0 & \\sin(\\beta) & \\cos(\\beta) \\end{array}\\right] \\]";
 
 var rotYEq = document.getElementById('rotYEq');
-rotYEq.innerHTML = "\\[ R_Y(\\beta) = \\left[ \\begin{array}{ccc} \\cos(\\beta) & 0 & \\sin(\\beta) \\\\ 0 & 1 & 0 \\\\ -\\sin(\\beta) & 0 & cos(\\beta) \\end{array}\\right] \\]";
+rotYEq.innerHTML = "\\[ R_Y(\\alpha) = \\left[ \\begin{array}{ccc} \\cos(\\alpha) & 0 & \\sin(\\alpha) \\\\ 0 & 1 & 0 \\\\ -\\sin(\\alpha) & 0 & cos(\\alpha) \\end{array}\\right] \\]";
 
 var rotZEq = document.getElementById('rotZEq');
-rotZEq.innerHTML = "\\[ R_Z(\\alpha) = \\left[ \\begin{array}{ccc} \\cos(\\alpha) & -\\sin(\\alpha) & 0 \\\\ \\sin(\\alpha) & \\cos(\\alpha) & 0 \\\\ 0 & 0 & 1 \\end{array}\\right] \\]";
+rotZEq.innerHTML = "\\[ R_Z(\\gamma) = \\left[ \\begin{array}{ccc} \\cos(\\gamma) & -\\sin(\\gamma) & 0 \\\\ \\sin(\\gamma) & \\cos(\\gamma) & 0 \\\\ 0 & 0 & 1 \\end{array}\\right] \\]";
